dc4e9ec813e29cea60c90f087673b07c229cc8c1
Minhaj28 <mdminhajuddin.official@gmail.com>
2023-03-24 02:55:12 +0600

    add consumer

--- src/App.js
+++ src/App.js
@@ -1,34 +1,35 @@
-import React, { useEffect, useState } from 'react';
-import './App.css';
-import { Input } from './component/input';
-import { Login } from './component/login';
-import { Video } from './component/videoPlayer';
-import UserContext from './contextExample/context/UserContext';
+import React, { useEffect, useState } from "react";
+import "./App.css";
+import { Input } from "./component/input";
+import { Login } from "./component/login";
+import { Video } from "./component/videoPlayer";
+import UserContext from "./contextExample/context/UserContext";
 
-import { Profile } from './contextExample/profile';
+import { Profile } from "./contextExample/profile";
+
+const Url = ({ href }) => <a href={href}>{href}</a>;
 
 function App() {
-  const [user,setuser] = useState();
+  const [user, setuser] = useState();
 
   useEffect(() => {
     setTimeout(() => {
       setuser({
-        id: '1234',
-        firstName: 'Minhaj',
-        lastName: 'Uddin',
+        id: "1234",
+        firstName: "Minhaj",
+        lastName: "Uddin",
         age: 24,
         address: {
-          country: 'Bangladesh',
-          city: 'Jashore',
+          country: "Bangladesh",
+          city: "Jashore",
         },
-        url: 'https://github.com/Minhaj28',
-      })
-      
+        url: "https://github.com/Minhaj28",
+      });
     }, 2000);
-  },[])
-    
-  if(!user){
-    return <div>Loading.....</div>
+  }, []);
+
+  if (!user) {
+    return <div>Loading.....</div>;
   }
   return (
     <div className="App">
@@ -37,6 +38,9 @@ function App() {
       {/* <Login/> */}
       <UserContext.Provider value={user}>
         <Profile />
+        <UserContext.Consumer>
+          {({ url }) => <Url href={url} />}
+        </UserContext.Consumer>
       </UserContext.Provider>
     </div>
   );
